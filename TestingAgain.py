c77f3115a27d2387371e034530c5c087e8f5580d
Bhupen <bhupenoffline@gmail.com>
2024-02-13 11:57:11 +0000

    commit

--- TestingAgain.py
+++ TestingAgain.py
@@ -1,1 +1,17 @@
-print("hihihi")
+names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry", "Ivy", "Jack",
+        "Katie", "Liam", "Mia", "Noah", "Olivia", "Peter", "Quinn", "Rachel", "Sam", "Tyler"]
+
+middleIndex = len(names) // 2
+
+valueWanted = input(f"Welcome to the value chooser!\nselect a value you want from the following list:\n{names}\nenter here:")
+
+for i in range(2, len(names) + 1):
+    if names[middleIndex] < valueWanted:
+        middleIndex += len(names) // (2 * i)
+    if names[middleIndex] > valueWanted:
+        middleIndex -= len(names) // (2 * i)
+    if names[middleIndex] == valueWanted:
+        print(f"{valueWanted} is found at index {middleIndex}")
+        break
+if names[middleIndex] != valueWanted:
+    print(f"{valueWanted} is not in the list")
