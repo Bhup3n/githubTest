323681a80bc68b978c92f936077cf9a9e25fe6dc
Bhupen <bhupenoffline@gmail.com>
2024-03-24 17:19:51 +0000

    commit and push

--- TestingAgain.py
+++ TestingAgain.py
@@ -1,16 +1,39 @@
-import math
+#This is hangman in Python
 
-def quadraticSolver(a, b, c, roundDecimal):
-    x1 = ((-(b)) + (math.sqrt(((b)**2) - (4*(a)*(c))))) / (2*(a))
-    x1Rounded = round(x1, roundDecimal)
-    x2 = ((-(b)) - (math.sqrt(((b)**2) - (4*(a)*(c))))) / (2*(a))
-    x2Rounded = round(x2, roundDecimal)
-    print(f"x = {x1} , x = {x2}")
-    print(f"x = {x1Rounded} , x = {x2Rounded}")
+import random
 
-a = float(input("what is the value of a: "))
-b = float(input("what is the value of b: "))
-c = float(input("what is the value of c: "))
-roundDecimal = int(input("how many dp do you want to round to: "))
+words = ['apple', 'banana', 'elephant', 'guitar', 'pumpkin', 'dolphin', 'rainbow', 'chocolate', 'firefly', 'pineapple']
 
-quadraticSolver(a, b, c, roundDecimal)
+word = random.choice(words)
+wordLength = len(word)
+wordObscured = "_" * wordLength
+
+incorrectGuesses = 0
+guessMade = []
+
+while incorrectGuesses <= 5:
+    print(f"\nthe word looks like the following: {wordObscured}")
+
+    if wordObscured == word:
+        print(f"\ncongratulations, you have guessed the word {word} correctly with {incorrectGuesses} wrong guesses!")
+        break
+
+    guess = input("guess a letter to be in the word: ").lower()
+
+    if guess in guessMade:
+        print(f"you have already guessed the letter {guess}! You cannot guess this letter again, try another letter")
+    else:
+        for i in range(0, wordLength):
+            if guess == word[i]:
+                wordObscuredList = list(wordObscured)
+                wordObscuredList[i] = guess
+                wordObscured = "".join(wordObscuredList)
+                print(f"oh nice! You guessed the letter {guess} correctly")
+
+        if guess not in wordObscured:
+            incorrectGuesses += 1
+            print(f"unfortunately the leter {guess} was not in the word. You now have {incorrectGuesses} wrong guesses")
+    guessMade.append(guess)
+
+if wordObscured != word:
+    print(f"\nthe closest you got was {wordObscured}\nthe word was: {word}")
